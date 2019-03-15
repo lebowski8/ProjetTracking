672be5f73aec6c6b57e249315aca74a16707e0ab
BahammouMohamed <bahammou.med11@gmail.com>
2019-03-15 13:42:31 +0100

    JpaRepository pour Utilisateur et CoordonneesGPS

---
+++ TrackingAppAPI/src/main/java/com/trackingapp/dao/CoordonneesGPSRepository.java
@@ -0,0 +1,11 @@
+package com.trackingapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trackingapp.model.CoordonneesGPS;
+
+
+
+public interface CoordonneesGPSRepository extends JpaRepository<CoordonneesGPS, Long>{
+
+}
---
+++ TrackingAppAPI/src/main/java/com/trackingapp/dao/UtilisateurRepository.java
@@ -0,0 +1,12 @@
+package com.trackingapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trackingapp.model.Utilisateur;
+
+
+
+
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long>{
+
+}
