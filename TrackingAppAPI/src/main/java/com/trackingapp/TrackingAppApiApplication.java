16185c028d7243fad738557a5377d3420cff96d2
BahammouMohamed <bahammou.med11@gmail.com>
2019-03-15 10:39:25 +0100

    Premier commit BackEnd

---
+++ TrackingAppAPI/src/main/java/com/trackingapp/TrackingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.trackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingAppApiApplication.class, args);
+	}
+
+}
---
+++ TrackingAppAPI/src/test/java/com/trackingapp/TrackingAppApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trackingapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrackingAppApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
