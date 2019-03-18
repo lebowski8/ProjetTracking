752ec07902518cbc4070c55c07ada5a76ad9dfbc
BahammouMohamed <bahammou.med11@gmail.com>
2019-03-18 10:29:54 +0100

    commit UtilisateurService

--- TrackingAppAPI/src/main/java/com/trackingapp/services/UtilisateurService.java
+++ TrackingAppAPI/src/main/java/com/trackingapp/services/UtilisateurService.java
@@ -3,22 +3,24 @@ package com.trackingapp.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trackingapp.dao.UtilisateurRepository;
+import com.trackingapp.exceptions.ResourceNotFoundException;
 import com.trackingapp.model.Utilisateur;
-
-
 
 
 
 @RestController
 @CrossOrigin("*")
 public class UtilisateurService {
+	
 	@Autowired
 	private UtilisateurRepository utilisateurRepository;
 	
@@ -35,38 +37,35 @@ public class UtilisateurService {
 		user.setPassword("");
         return user;
     }
-	
+			
+	@RequestMapping(value="/users",method=RequestMethod.POST)
 	public Utilisateur createUser( Utilisateur user) throws Exception{
 		Utilisateur usertmp = utilisateurRepository.findByUsername(user.getUsername());
 		if(usertmp!=null)
 			throw new Exception("Pseudo existe déja");
-		
 		return utilisateurRepository.save(user);
     }
 
-	/*@RequestMapping(value="/users/{id}",method=RequestMethod.DELETE)
+	@RequestMapping(value="/users/{id}",method=RequestMethod.DELETE)
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
-		return userRepository.findById(id).map(user -> {
-			userRepository.delete(user);
+		return utilisateurRepository.findById(id).map(user -> {
+			utilisateurRepository.delete(user);
 	        return ResponseEntity.ok().build();
 	    }).orElseThrow(() -> new ResourceNotFoundException("UserID " + id + " not found"));
 
     }
 	
 	@RequestMapping(value="/users/{id}",method=RequestMethod.PUT)
-    public User updateUser(@PathVariable Long id,@RequestBody User userRequest) {
-		return userRepository.findById(id).map(user -> {
+    public Utilisateur updateUser(@PathVariable Long id,@RequestBody Utilisateur userRequest) {
+		return utilisateurRepository.findById(id).map(user -> {
 			user.setNom(userRequest.getNom());
 			user.setPrenom(userRequest.getPrenom());
-			user.setPseudo(userRequest.getPseudo());
-			String hashPW=bCryptPasswordEncoder.encode(userRequest.getPassword());
-			user.setPassword(hashPW);
-			user.setEmail(userRequest.getEmail());
-			user.setStatus(userRequest.getStatus());
-			user.setValidated(false);
-	        return userRepository.save(user);
+			user.setUsername(userRequest.getUsername());
+			user.setPassword(userRequest.getPassword());
+			user.setPoids(userRequest.getPoids());
+	        return utilisateurRepository.save(user);
 	    }).orElseThrow(() -> new ResourceNotFoundException("UserID " + id + " not found"));
 
-    }*/
+    }
 	
 }
